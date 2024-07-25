365adb7b524b98f6e8335ad67ca7ce1394f01dfb
Arghya <arghyadutta080@gmail.com>
2024-07-25 21:43:12 +0530

    update: sells routes

--- server/controllers/sells.py
+++ server/controllers/sells.py
@@ -1,5 +1,7 @@
 from fastapi import HTTPException, status
 from sqlalchemy.orm import Session
+from sqlalchemy import select, func
+from datetime import date
 from ..models import Order, Sell
 from ..schemas import user, sell
 
@@ -8,13 +10,19 @@ def mark_as_delivered(request: sell.SellCreate, user: user.User, db: Session):
     if user.role == "ADMIN":
         if (request.id and request.delivery_date):
             order = db.query(Order).filter(Order.id == request.id)
-            order_data = order.first()
-            if order_data is None:
+            order_data = order.all()
+
+            if len(order_data) == 0:
                 raise HTTPException(
                     status_code=status.HTTP_404_NOT_FOUND, detail="No such order in database")
+            
+            elif order_data[0].delivered is True:
+                raise HTTPException(
+                    status_code=status.HTTP_306_RESERVED, detail="Ordered is already delivered")
             else:
                 order.update({"delivered": True})
-                delivered_order = Sell(id=request.id, delivery_date=request.delivery_date)
+                delivered_order = Sell(
+                    id=request.id, delivery_date=request.delivery_date)
                 db.add(delivered_order)
                 db.commit()
                 db.refresh(delivered_order)
@@ -30,6 +38,40 @@ def mark_as_delivered(request: sell.SellCreate, user: user.User, db: Session):
 def get_sells(user: user.User, db: Session):
     if user.role == "ADMIN":
         all_sells = db.query(Sell).all()
+
+        if all_sells is None:
+            raise HTTPException(
+                status_code=status.HTTP_204_NO_CONTENT, detail="No Selling data in database")
+        else:
+            return all_sells
+    else:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
+                            detail="You are not authorized for this action")
+
+
+def get_datewise_sell_count(user: user.User, db: Session):
+    if user.role == "ADMIN":
+        sells_data = select(Sell.delivery_date, func.sum(Order.quantity)).select_from(
+            Sell).join(Order, Order.id == Sell.id).group_by(Sell.delivery_date)
+        sells_data = db.execute(sells_data).all()
+
+        sells_info = [{'date': date, 'total_sell': total_sell}
+                      for date, total_sell in sells_data]
+
+        if sells_info is None:
+            raise HTTPException(
+                status_code=status.HTTP_204_NO_CONTENT, detail="No Selling data in database")
+        else:
+            return sells_info
+    else:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
+                            detail="You are not authorized for this action")
+
+
+def get_datewise_sells(user: user.User, db: Session, date: str | date):
+    if user.role == "ADMIN":
+        all_sells = db.query(Sell).filter(Sell.delivery_date == date).all()
+
         if all_sells is None:
             raise HTTPException(
                 status_code=status.HTTP_204_NO_CONTENT, detail="No Selling data in database")
--- server/routes/sells.py
+++ server/routes/sells.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 from typing import Annotated
+from datetime import date
 from ..utils import getDB
 from ..schemas import user, sell
 from ..controllers import sells
@@ -16,5 +17,15 @@ def mark_as_delivered(request: sell.SellCreate, current_user: Annotated[user.Use
 
 
 @router.get('/all', response_model=list[sell.Sell])
-def view_all_sells(current_user: Annotated[user.User, Depends(users.get_user_profile)], db: Session = Depends(getDB.get_db)):
+def view_sells_info(current_user: Annotated[user.User, Depends(users.get_user_profile)], db: Session = Depends(getDB.get_db)):
     return sells.get_sells(user=current_user, db=db)
+
+
+@router.get('/date/count')
+def view_datewise_total_sell(current_user: Annotated[user.User, Depends(users.get_user_profile)], db: Session = Depends(getDB.get_db)):
+    return sells.get_datewise_sell_count(user=current_user, db=db)
+
+
+@router.get('/info', response_model=list[sell.Sell])
+def view_datewise_sells_info(current_user: Annotated[user.User, Depends(users.get_user_profile)], db: Session = Depends(getDB.get_db), date: str | date = date.today()):
+    return sells.get_datewise_sells(user=current_user, db=db, date=date)
